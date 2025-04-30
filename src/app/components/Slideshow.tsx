d7ca12e897da9d619a5b56330c5f1aa165e519fe
AntMatter <mswonger@colostate.edu>
2025-04-30 01:25:27 -0600

    Fix image build errors

--- src/app/components/Slideshow.tsx
+++ src/app/components/Slideshow.tsx
@@ -2,7 +2,6 @@
 import { IconButton } from "@mui/material";
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
 import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import Image from "next/image"
 import React, { useState } from "react"
 
 import '../styles.css'
